fc2ae89f76e1b6a9420b00f55e8f4958f6fb23e7
Peter Heywood <peethwd@gmail.com>
2021-03-04 19:40:56 +0000

    Reduce number of csv files generated to two

--- src/main.cu
+++ src/main.cu
@@ -31,42 +31,6 @@ void run_circles_spatial3D_rtc(const RunSimulationInputs runInputs, RunSimulatio
 
 void print_cli_help(const int argc, const char ** argv );
 custom_cli parse_custom_cli(const int argc, const char ** argv);
-    
-
-__global__ void warmup(float * data, uint32_t elements, uint32_t reps) {
-
-    for(uint32_t idx = threadIdx.x + blockIdx.x * blockDim.x; idx < elements; idx += blockDim.x * gridDim.x){
-        data[idx] = idx;
-        for(uint32_t rep = 0; rep < reps; rep++){
-            data[idx] = data[idx] + ((threadIdx.x * rep) % blockDim.x);
-        }
-    }
-
-}
-
-// Do some arbitrary work on the device to get it  itno a different power state.
-void cudaWarmup() {
-    // @todo - cuda check.
-    const uint32_t elements = 2 << 20; 
-    const size_t bytes = elements * sizeof(float);
-    const uint32_t reps = 2 << 8; 
-
-    float * d_data = nullptr;
-    cudaMalloc(&d_data, bytes);
-    cudaMemset(d_data, 0, bytes);
-
-    int blockSize = 256;
-    int gridSize = (elements + blockSize - 1) / blockSize;
-
-    warmup<<<gridSize, blockSize, 0, 0>>>(d_data, elements, reps);
-    
-    cudaFree(d_data);
-    d_data = nullptr;
-    
-
-
-
-}
 
 void printProgress(const std::string modelName, const uint32_t count, const uint32_t total, const uint32_t agentCount, const uint32_t repeat){
     std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
@@ -75,7 +39,7 @@ void printProgress(const std::string modelName, const uint32_t count, const uint
     fprintf(stdout, "%s: %u/%u: %s %u %u\n", buf, count + 1, total, modelName.c_str(), agentCount, repeat);
 }
 
-
+// @todo - actual device poower state warmup? Maybe run the 0th sim twice and only use the second one?
 // @todo deal with what happens if a simulation throws an exception?
 int main(int argc, const char ** argv) {
     // Custom arg parsing, to prevent the current F2 arg parsing from occuring. 
@@ -87,7 +51,7 @@ int main(int argc, const char ** argv) {
     std::vector<uint32_t> POPULATION_SIZES = {
         2u << 8,
         2u << 9,
-        2u << 10,
+        // 2u << 10,
         // 2u << 11,
         // 2u << 12,
         // 2u << 13,
@@ -120,11 +84,25 @@ int main(int argc, const char ** argv) {
         exit(EXIT_FAILURE);
     }
     
-    
     // Output the header for the per run timing.
     fprintf(fp_rowPerSimulation, "GPU,release_mode,seatbelts,model,steps,agentCount,repeat,ms_rtc,ms_simulation,ms_init,ms_exit,ms_stepMean\n");
     
+    /* std::string s = std::string("");
+    for(uint32_t step = 0; step < cli.steps; step++){
+        s += std::string(",ms_step_") + std::to_string(step);
+    }
+    fprintf(fp_rowPerSimulation, "%s\n", s.c_str()); */
 
+
+    // Write a row per step out to  a differnt file.
+    std::FILE * fp_rowPerStepPerSimulation = std::fopen("row-per-step-per-simulation.csv", "w");
+    if(fp_rowPerSimulation == nullptr){
+        printf("could not open file? @todo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Output a header row. 
+    fprintf(fp_rowPerStepPerSimulation, "GPU,release_mode,seatbelts,model,steps,agentCount,repeat,step,ms_step\n");
     
     // Get the name of the gpu. 
     std::string deviceName("unknown");
@@ -138,7 +116,6 @@ int main(int argc, const char ** argv) {
             if (cudaSuccess == status) {
                 deviceName = std::string(props.name);
                 cudaFree(0); // Make a context / slightly warm the decice?
-                cudaWarmup();
             } else {
                 printf("@todo handle error \n");
             }
@@ -162,21 +139,12 @@ int main(int argc, const char ** argv) {
         // @todo - do (some) aggregation of timers? e.g. output 1 csv for a given sim-popsize combo, with step times for each sim and mean step times? for 
         
         // Iterate over the population sizes for that model
-        for(auto const& agentCount : POPULATION_SIZES){            
+        for(auto const& agentCount : POPULATION_SIZES){     
             // Repeat a number of times to get an average.
             for(uint32_t repeat = 0u; repeat < cli.repetitions; repeat++) { 
 
                 // Progress. 
                 printProgress(modelName, counter, totalSimulations, agentCount, repeat);
-
-                // @todo - better filenaming
-                std::string simulationStepsRawFilename = std::string("simulation-steps-raw-") + std::to_string(counter) + std::string(".csv");
-                // @todo better file handling, directory etc.
-                std::FILE * fp_simulationStepsRaw = std::fopen(simulationStepsRawFilename.c_str(), "w");
-                if(fp_simulationStepsRaw == nullptr){
-                    printf("could not open file? @todo\n");
-                    exit(EXIT_FAILURE);
-                }
                 // @todo - Do i need to use a different seed for each repetition? Probably should do both...
                 const uint64_t seed = cli.seed;
                 
@@ -187,23 +155,20 @@ int main(int argc, const char ** argv) {
                 modelFunction(runInputs, runOutputs);
 
 
-                // Output the individual runs times
-                fprintf(fp_rowPerSimulation, "\"%s\",%d,%d,\"%s\",%u,%u,%u,%.3f,%.3f,%.3f,%.3f,%.3f\n", deviceName.c_str(), RELEASE_MODE, SEATBELTS_ON, modelName.c_str(), cli.steps, agentCount, repeat, runOutputs.ms_rtc, runOutputs.ms_simulation, runOutputs.ms_init, runOutputs.ms_exit, runOutputs.ms_stepMean); 
+                // Add a row to the row per simulation csv file
+                fprintf(fp_rowPerSimulation, "%s,%d,%d,%s,%u,%u,%u,%.3f,%.3f,%.3f,%.3f,%.3f\n", deviceName.c_str(), RELEASE_MODE, SEATBELTS_ON, modelName.c_str(), cli.steps, agentCount, repeat, runOutputs.ms_rtc, runOutputs.ms_simulation, runOutputs.ms_init, runOutputs.ms_exit, runOutputs.ms_stepMean); 
                 
-                // Output a csv containing the per step time 
-                fprintf(fp_simulationStepsRaw, "GPU,release_mode,seatbelts,model,steps,agentCount,repeat,step,ms_step\n");
+                // add a row to the row per step per simulation file for each step. This is wasting a lot of disk space... 
                 for(uint32_t step = 0; step < runOutputs.ms_per_step->size(); step++){
                     auto& ms_step = runOutputs.ms_per_step->at(step);
-                    fprintf(fp_simulationStepsRaw, "\"%s\",%d,%d,\"%s\",%u,%u,%u,%u,%.3f\n", deviceName.c_str(), RELEASE_MODE, SEATBELTS_ON, modelName.c_str(), cli.steps, agentCount, repeat, step, ms_step); 
+                    fprintf(fp_rowPerStepPerSimulation, "%s,%d,%d,%s,%u,%u,%u,%u,%.3f\n", deviceName.c_str(), RELEASE_MODE, SEATBELTS_ON, modelName.c_str(), cli.steps, agentCount, repeat, step, ms_step); 
                 }
-
-                std::fclose(fp_simulationStepsRaw);
-                fp_simulationStepsRaw = nullptr;
-
                 counter++;
             }
         }
     }
+    std::fclose(fp_rowPerStepPerSimulation);
+    fp_rowPerStepPerSimulation = nullptr;
     std::fclose(fp_rowPerSimulation);
     fp_rowPerSimulation = nullptr;
 }
