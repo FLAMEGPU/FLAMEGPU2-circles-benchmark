77a1959424a35b3895e39be0180908f1fe233798
Peter Heywood <p.heywood@sheffield.ac.uk>
2021-03-08 12:15:17 +0000

    Add slurm job submisssion scripts for Bessemer

---
+++ scripts/slurm/compile.bessemer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=4
+#SBATCH --job-name=compile.bessemer.sh
+
+# Load modules for compilation
+module load CUDA/10.2.89-GCC-8.3.0 
+
+# Load cmake with matching glibc version. Alternatively use a cmake installed via pip into a local venv.
+module load CMake/3.15.3-GCCcore-8.3.0
+
+# Load matching python too 
+# module load Python/3.7.4-GCCcore-8.3.0
+
+# Make tmpdir incase it doesn't exist? This should not be neccesary...
+mkdir -p $TMPDIR
+
+# Set the location of the project root relative to this script
+PROJECT_ROOT=../..
+
+# navigate into the root directory.
+cd $PROJECT_ROOT
+
+# Make the build directory.
+mkdir -p build && cd build
+
+# Configure cmake.
+cmake .. -DCUDA_ARCH=70 -DCMAKE_BUILD_TYPE=Release -DSEATBELTS=OFF 
+
+# Compile the code with make for GPUs in Bessener (SM_70)
+make -j `nproc`
+
---
+++ scripts/slurm/run.bessemer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Will need to increase the time for full benchmark runs.... 
+#SBATCH --time=8:00:00
+#SBATCH --partition=dcs-gpu
+#SBATCH --account=dcs-res
+# 1 GPU
+#SBATCH --gres=gpu:1
+# 1 CPU core - might want to increase this + memory to 1/4 of the node per GPU?
+#SBATCH --ntasks=1
+
+# Load modules (with matching gcc versions)
+module load CUDA/10.2.89-GCC-8.3.0 
+# module load CMake/3.15.3-GCCcore-8.3.0
+# module load Python/3.7.4-GCCcore-8.3.0
+
+
+# Make tmpdir incase it doesn't exist? This should not be neccesary...
+mkdir -p $TMPDIR
+
+# Set the location of the project root relative to this script
+PROJECT_ROOT=../..
+
+# navigate into the `build` directory.
+cd $PROJECT_ROOT
+cd build
+
+# Output some GPU information into the Log
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
+
+# Run the executable.
+./bin/linux-x64/Release/circles-benchmarking
