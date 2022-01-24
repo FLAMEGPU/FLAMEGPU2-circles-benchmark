2e504af4b7d607a4985bafe078ac6d3e3fa136ac
Matthew Leach <matt-leach@hotmail.co.uk>
2022-01-24 12:19:32 +0000

    Only run variable com radius

--- src/circles_bruteforce_rtc_sorted.cu
+++ src/circles_bruteforce_rtc_sorted.cu
@@ -93,8 +93,8 @@ FLAMEGPU_STEP_FUNCTION(Validation) {
 }  // namespace
 
 // Run an individual simulation, using 
-void run_circles_bruteforce_sorted_rtc(const RunSimulationInputs runInputs, RunSimulationOutputs &runOutputs){
-    flamegpu::ModelDescription model("circles_bruteforce_sorted_rtc");
+void run_circles_bruteforce_rtc_sorted(const RunSimulationInputs runInputs, RunSimulationOutputs &runOutputs){
+    flamegpu::ModelDescription model("circles_bruteforce_rtc_sorted");
     // Calculate environment bounds.
     const float ENV_WIDTH = runInputs.ENV_WIDTH;
     const float ENV_MIN = -0.5 * ENV_WIDTH;
