2e8b44737a99467b48e2cc3ed40ae9990a2ce695
Peter Heywood <peethwd@gmail.com>
2022-05-26 15:40:36 +0100

    Don't change dir in to job script, slurm breaks it

--- scripts/slurm/compile.a100-tmp.sh
+++ scripts/slurm/compile.a100-tmp.sh
@@ -21,9 +21,6 @@ module load GCC/11.2.0
 module load CUDA/11.4.1
 module load CMake/3.21.1-GCCcore-11.2.0
 
-# CD into the script's directory, so the relative path exists when not exeucted as a batch job
-cd "$(dirname "$0")"
-
 # Set the location of the project root relative to this script
 PROJECT_ROOT=../..
 
